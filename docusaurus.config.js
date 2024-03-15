f33073b815624bafef1ea82cac11d1fe79793784
XIA Yunlei <mark.xia@qfpay.com>
2024-03-15 12:19:26 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   organizationName: 'yxiaao', // Usually your GitHub org/user name.
   projectName: 'qfpay-sdk', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
