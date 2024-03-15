d943cf4ae63116638b723c77a3db7571766da620
XIA Yunlei <mark.xia@qfpay.com>
2024-03-15 15:54:48 +0800

    Update index.js

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -6,27 +6,27 @@ import HomeCard from '../HomepageCard';
 const FeatureList = [
   {
     title: 'In-Store',
-    Svg: '/qf-sdk/img/in-store.png',
+    Svg: '/qfpay-sdk/img/in-store.png',
     description: (
       <>
         Experience swift, secure shopping with our in-store payment solution. Accepts all major cards and e-wallets. Quick, convenient, and crafted for you.
       </>
     ),
     links:[
-      {title:"POS Integration", url:"/qf-sdk/docs/category/cdi"},
+      {title:"POS Integration", url:"/qfpay-sdk/docs/category/cdi"},
       {title:"In-Store common API -MPQR & CPQR", url:"/docs/category/cdi"}
     ]
   },
   {
     title: 'Online Shop',
-    Svg: '/qf-sdk/img/online-shop.png',
+    Svg: '/qfpay-sdk/img/online-shop.png',
     description: (
       <>
         Enjoy secure, instant online payments with our solution. Supports all major online scenarios and e-wallets / credit cards. Fast, flexible, and designed for your convenience.
       </>
     ),
     links:[
-      {title:"Checkout Services", url:"/qf-sdk/docs/category/cdi"},
+      {title:"Checkout Services", url:"/qfpay-sdk/docs/category/cdi"},
       {title:"Embedded Component", url:""},
       {title:"Recurring Payment", url:""},
       {title:"Official Accounts/ Mini Programs", url:""}
@@ -34,14 +34,14 @@ const FeatureList = [
   },
   {
     title: 'E-Commerce',
-    Svg: '/qf-sdk/img/e-commerce.png',
+    Svg: '/qfpay-sdk/img/e-commerce.png',
     description: (
       <>
         Boost your online store payment abilities with our e-commerce plugins for Shopify, WooCommerce, OpenCart, and Magento. Easy to integrate, reliable, and designed to enhance your business.
       </>
     ),
     links:[
-      {title:"POS Integration", url:"/qf-sdk/docs/category/cdi"}
+      {title:"POS Integration", url:"/qfpay-sdk/docs/category/cdi"}
     ]
   },
 ];
